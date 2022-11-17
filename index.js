5baa50a1a24883068718bd0c40ba803f7054661f
devendraprasad1984@gmail.com <devendraprasad1984@gmail.com>
2022-11-17 10:44:45 +0530

    update

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {createRoot} from 'react-dom/client'
+
+
+const appContainer = document.getElementById('root')
+function App(){
+    return <React.Fragment>
+        <div>Hello World!</div>
+    </React.Fragment>
+}
+const Root = createRoot(appContainer)
+Root.render(App)
---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+import livereload from "rollup-plugin-livereload";
+import uglify from "rollup-plugin-uglify";
+import commonjs from "rollup-plugin-commonjs";
+import replace from "@rollup/plugin-replace";
+import serve from "rollup-plugin-serve";
+
+const NODE_ENV = process.env.NODE_ENV || 'development'
+export default {
+    input: "index.js",
+    output: {
+        file: "bundle/bundle.js",
+        format: "cjs"
+    },
+    plugins: [
+        replace({
+            "process.env.NODE_ENV": JSON.stringify(NODE_ENV),
+            preventAssignment: true
+        }),
+        babel({
+            exclude: "node_modules/**"
+        }),
+        resolve(),
+        commonjs(),
+        NODE_ENV !== 'production' && serve({contentBase: ''}),
+        NODE_ENV !== 'production' && livereload(),
+        NODE_ENV === 'production' && uglify(),
+    ]
+}
